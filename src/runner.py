86b8ed8e9ecf1b81831124c657834aea61a949a4
Stef McCuistion <stef.mccuistion@gmail.com>
2025-01-02 11:58:11 -0600

    Added text and moving snail

--- src/runner.py
+++ src/runner.py
@@ -11,17 +11,27 @@ def main():
     window = pg.display.set_mode((W, H))
     pg.display.set_caption('Runner')
     clock = pg.time.Clock()
+    font = pg.font.Font('runner/font/pixeltype.ttf', 50)
 
-    bg = pg.image.load('runner/graphics/sky.png')
-    ground = pg.image.load('runner/graphics/ground.png')
+    bg_surf = pg.image.load('runner/graphics/sky.png')
+    ground_surf = pg.image.load('runner/graphics/ground.png')
+    txt_surf = font.render("Stef's Game", False, 'Black')
+
+    snail_surf = pg.image.load('runner/graphics/snail/snail1.png')
+    snail_x = 600
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 exit()
-        screen.blit(bg)
-        screen.blit(ground, (0, 300))
+        screen.blit(bg_surf)
+        screen.blit(ground_surf, (0, 300))
+        screen.blit(txt_surf, (300, 50))
+        screen.blit(snail_surf, (snail_x, 264))
+        snail_x -= 4
+        if snail_x < -72:
+            snail_x = screen.get_width()
         pg.transform.scale(screen, (W, H), window)
         pg.display.update()
         clock.tick(60)
