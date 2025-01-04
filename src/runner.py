54f30082d9923e1d3721e231456cde3be996ecd4
Stef McCuistion <stef.mccuistion@gmail.com>
2025-01-03 18:46:26 -0600

    Followed tutorial up to 1:41, jumping is implemented

--- src/runner.py
+++ src/runner.py
@@ -3,19 +3,17 @@ from sys import exit
 
 def main():
     pg.init()
-    screen = pg.Surface((800, 400))
-    W, H = (
-            1600, 
-            800,
-    )
-    window = pg.display.set_mode((W, H))
+    screen = pg.display.set_mode((800, 400))
     pg.display.set_caption('Runner')
     clock = pg.time.Clock()
     font = pg.font.Font('runner/font/pixeltype.ttf', 50)
 
     bg_surf = pg.image.load('runner/graphics/sky.png').convert()
     ground_surf = pg.image.load('runner/graphics/ground.png').convert()
-    txt_surf = font.render("Stef's Game", False, 'Black').convert()
+
+    txt_surf = font.render("Stef's Game", False, (64, 64, 64)).convert()
+    txt_rect = txt_surf.get_rect(midbottom = (400, 70))
+    txt_backdrop_rect = txt_rect.inflate(10, 10)
 
     snail_surf = pg.image.load('runner/graphics/snail/snail1.png').convert_alpha()
     snail_rect = snail_surf.get_rect(midbottom = (700, 300))
@@ -23,20 +21,32 @@ def main():
     player_surf = pg.image.load('runner/graphics/player/player_walk_1.png').convert_alpha()
     player_rect = player_surf.get_rect(midbottom = (80, 300))
 
+    gravity = -20
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    gravity = -20
         screen.blit(bg_surf)
         screen.blit(ground_surf, (0, 300))
-        screen.blit(txt_surf, (300, 50))
+        pg.draw.rect(screen, '#c0e8ec', txt_backdrop_rect, False, 5)
+        screen.blit(txt_surf, txt_rect)
+
         screen.blit(snail_surf, snail_rect)
-        screen.blit(player_surf, player_rect)
+
         snail_rect.left -= 4
-        if snail_rect.left < -snail_rect.width:
+        if snail_rect.right < 0:
             snail_rect.left = screen.get_width()
-        pg.transform.scale(screen, (W, H), window)
+
+        gravity += 1
+        screen.blit(player_surf, player_rect)
+        player_rect.y += gravity
+        player_rect.colliderect(snail_rect)
+
         pg.display.update()
         clock.tick(60)
 
