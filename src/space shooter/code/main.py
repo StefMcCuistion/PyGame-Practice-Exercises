f4738b3c22845ecdf961dc8dd4a0beb860f7edc5
Stef McCuistion <stef.mccuistion@gmail.com>
2025-01-04 04:54:46 -0600

    Renamed 'space shooter" to "space_shooter". Initialized, added while loop, event loop, quit, and display

--- src/space shooter/code/main.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-def main():
-    # General setup
-    pygame.init()
-
-if __name__ == "__main__":
-    main()
---
+++ src/space_shooter/code/main.py
@@ -0,0 +1,26 @@
+import pygame as pg
+
+def main():
+    # General setup
+    pg.init()
+    W, H = (
+            1280,
+            720
+    ) # Window width and height
+    display = pg.display.set_mode((W, H))
+    running = True
+
+    while running: 
+        # Event loop
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        # Draw the game
+        # Fill window with red
+        display.fill('red')
+        pg.display.flip()
+    pg.quit()
+
+if __name__ == "__main__":
+    main()
