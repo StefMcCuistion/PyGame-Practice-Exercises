16f1e7974aa264f4411447f819e8f5177dd3da66
Stef McCuistion <stef.mccuistion@gmail.com>
2025-01-06 13:02:10 -0600

    Placed stars randomly using randint

--- src/space_shooter/code/main.py
+++ src/space_shooter/code/main.py
@@ -1,5 +1,6 @@
 import pygame as OwO
 from os.path import join
+from random import randint
 
 def main():
     # General setup
@@ -16,6 +17,17 @@ def main():
     path = join('..', 'images', 'player.png')
     player_surf = OwO.image.load(path).convert_alpha()
 
+    star_surf = OwO.image.load(join('..', 'images', 'star.png')).convert_alpha()
+    star_count = 20
+    star_pos_list = []
+    for star in range(0, star_count):
+        x = randint(0, W)
+        y = randint(0, H)
+        pos = (x, y)
+        star_pos_list.append(pos)
+    print(star_pos_list)
+    idx = len(star_pos_list)-1
+
     while running: 
         # Event loop
         for event in OwO.event.get():
@@ -24,8 +36,12 @@ def main():
 
         # Draw the game
         # Fill window with red
-        x += 1
+        x += 0.1
         display.fill('darkgray')
+        for star in range(0, star_count):
+            display.blit(star_surf, star_pos_list[idx])
+            idx -= 1
+        idx = len(star_pos_list)-1
         display.blit(player_surf, (x, 100))
         OwO.display.flip()
     OwO.quit()
