1c2ea5520ab60477999e806b87785f653a841631
Stef McCuistion <stef.mccuistion@gmail.com>
2025-01-06 15:43:57 -0600

    Implemented deltatime. Made ship bounce around the screen like a screensaver.

--- src/space_shooter/code/main.py
+++ src/space_shooter/code/main.py
@@ -11,6 +11,7 @@ def main():
     ) # Window width and height
     display = OwO.display.set_mode((W, H))
     OwO.display.set_caption('Space Shooter')
+    clock = OwO.time.Clock()
     running = True
 
     # Imports
@@ -18,7 +19,8 @@ def main():
     path = join('..', 'images', 'player.png')
     player_surf = OwO.image.load(path).convert_alpha()
     player_rect = player_surf.get_frect(bottomleft = (10, H - 10))
-    player_dir = 1
+    player_dir = OwO.math.Vector2(2, -1)
+    player_speed = 250
 
     ## Meteor
     meteor_surf = OwO.image.load(join('..', 'images', 'meteor.png')).convert_alpha()
@@ -26,13 +28,14 @@ def main():
 
     ## Lazer
     lazer_surf = OwO.image.load(join('..', 'images', 'laser.png')).convert_alpha()
-    lazer_rect = lazer_surf.get_frect(bottomleft = (W - 20, H - 20))
+    lazer_rect = lazer_surf.get_frect(bottomleft = (20, H - 20))
 
     ## Stars
     star_surf = OwO.image.load(join('..', 'images', 'star.png')).convert_alpha()
     star_positions = [(randint(0, W), randint(0, H)) for i in range(20)]
 
-    while running: 
+    while running:
+        dt = clock.tick(120) / 1000
         # Event loop
         for event in OwO.event.get():
             if event.type == OwO.QUIT:
@@ -44,10 +47,16 @@ def main():
             display.blit(star_surf, pos)
 
         display.blit(lazer_surf, lazer_rect)
-
-        if player_rect.right == W or player_rect.left == 0:
-            player_dir = -player_dir
-        player_rect.left += .5*player_dir
+        ## Player movement
+        player_rect.center += player_dir * player_speed * dt
+        if player_rect.top <= 0:
+            player_dir.y *= -1
+        if player_rect.bottom >= H:
+            player_dir.y *= -1
+        if player_rect.left <= 0:
+            player_dir.x *= -1
+        if player_rect.right >= W:
+            player_dir.x *= -1
         display.blit(player_surf, player_rect)
 
         display.blit(meteor_surf, meteor_rect)
