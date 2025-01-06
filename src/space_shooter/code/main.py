5aae8f71fa7972b52babf3787aca1271c84573db
Stef McCuistion <stef.mccuistion@gmail.com>
2025-01-06 13:07:07 -0600

    Switched star placement to the MUCH better method shown in the tutorial

--- src/space_shooter/code/main.py
+++ src/space_shooter/code/main.py
@@ -18,15 +18,7 @@ def main():
     player_surf = OwO.image.load(path).convert_alpha()
 
     star_surf = OwO.image.load(join('..', 'images', 'star.png')).convert_alpha()
-    star_count = 20
-    star_pos_list = []
-    for star in range(0, star_count):
-        x = randint(0, W)
-        y = randint(0, H)
-        pos = (x, y)
-        star_pos_list.append(pos)
-    print(star_pos_list)
-    idx = len(star_pos_list)-1
+    star_positions = [(randint(0, W), randint(0, H)) for i in range(20)]
 
     while running: 
         # Event loop
@@ -38,10 +30,8 @@ def main():
         # Fill window with red
         x += 0.1
         display.fill('darkgray')
-        for star in range(0, star_count):
-            display.blit(star_surf, star_pos_list[idx])
-            idx -= 1
-        idx = len(star_pos_list)-1
+        for pos in star_positions:
+            display.blit(star_surf, pos)
         display.blit(player_surf, (x, 100))
         OwO.display.flip()
     OwO.quit()
