eaeb872a002f14b64d29c2b2f05c2b06f0caa9d7
Stef McCuistion <stef.mccuistion@gmail.com>
2025-01-07 10:21:24 -0600

    Added left to right player movement

--- src/space_shooter/code/main.py
+++ src/space_shooter/code/main.py
@@ -19,8 +19,8 @@ def main():
     path = join('..', 'images', 'player.png')
     player_surf = OwO.image.load(path).convert_alpha()
     player_rect = player_surf.get_frect(bottomleft = (10, H - 10))
-    player_dir = OwO.math.Vector2(2, -1)
-    player_speed = 250
+    player_dir = OwO.math.Vector2()
+    player_speed = 300
 
     ## Meteor
     meteor_surf = OwO.image.load(join('..', 'images', 'meteor.png')).convert_alpha()
@@ -41,6 +41,12 @@ def main():
             if event.type == OwO.QUIT:
                 running = False
 
+
+        # Input
+        keys = OwO.key.get_pressed()
+        player_dir.x = int(keys[OwO.K_RIGHT]) - int(keys[OwO.K_LEFT])
+        player_rect.center += player_dir * player_speed * dt
+
         # Draw the game
         display.fill('darkgray')
         for pos in star_positions:
@@ -48,15 +54,15 @@ def main():
 
         display.blit(lazer_surf, lazer_rect)
         ## Player movement
-        player_rect.center += player_dir * player_speed * dt
-        if player_rect.top <= 0:
-            player_dir.y *= -1
-        if player_rect.bottom >= H:
-            player_dir.y *= -1
-        if player_rect.left <= 0:
-            player_dir.x *= -1
-        if player_rect.right >= W:
-            player_dir.x *= -1
+        # player_rect.center += player_dir * player_speed * dt
+        # if player_rect.top <= 0:
+        #     player_dir.y *= -1
+        # if player_rect.bottom >= H:
+        #     player_dir.y *= -1
+        # if player_rect.left <= 0:
+        #     player_dir.x *= -1
+        # if player_rect.right >= W:
+        #     player_dir.x *= -1
         display.blit(player_surf, player_rect)
 
         display.blit(meteor_surf, meteor_rect)
