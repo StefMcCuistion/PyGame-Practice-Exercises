1b97adc8b5f6f118900ef72c2afb94a566163bee
StefMcCuistion <stef.mccuistion@gmail.com>
2025-01-20 19:46:56 -0600

    Drafted main while loop, event loop, player frame imports, movement. Player is not yet animated.

--- src/vampire-survivor/code/main.py
+++ src/vampire-survivor/code/main.py
@@ -17,6 +17,7 @@ class Game():
             self.dir.y = int(keys[pg.K_DOWN]) - int(keys[pg.K_UP])
             self.dir = self.dir.normalize() if self.dir else self.dir
             self.rect.center += self.dir * self.speed * dt
+
     
     # Setup
     pg.init()
@@ -26,4 +27,30 @@ class Game():
     running = True
 
     # Imports
-    player_frames = [pg.image.load(join('..', 'images', ))]
+    player_up_frames = [pg.image.load(join('..', 'images', 'player', 'up', f'{i}.png')).convert_alpha() for i in range(4)]
+    player_right_frames = [pg.image.load(join('..', 'images', 'player', 'right', f'{i}.png')).convert_alpha() for i in range(4)]
+    player_down_frames = [pg.image.load(join('..', 'images', 'player', 'down', f'{i}.png')).convert_alpha() for i in range(4)]
+    player_left_frames = [pg.image.load(join('..', 'images', 'player', 'left', f'{i}.png')).convert_alpha() for i in range(4)]
+
+    # Sprites
+    all_sprites = pg.sprite.Group()
+    player = Player(player_down_frames, all_sprites)
+
+    while running:
+        dt = clock.tick() / 1000
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    running = False
+
+        # Update
+        all_sprites.update(dt)
+
+        # Draw the game
+        display.fill('gray')
+        all_sprites.draw(display)
+
+        pg.display.flip()
+    pg.quit()
