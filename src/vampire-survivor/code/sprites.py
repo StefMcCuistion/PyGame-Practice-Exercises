7c0a129ec38f5baf1ba7ab3e98532c94913ef6ba
Stef McCuistion <stef.mccuistion@gmail.com>
2025-02-01 23:44:40 -0600

    Created Sprite class in sprites module. Code is identical to CollisionSprite but Sprite class will be noncollideable, used for the ground layer.

--- src/vampire-survivor/code/sprites.py
+++ src/vampire-survivor/code/sprites.py
@@ -1,5 +1,11 @@
 from settings import *
 
+class Sprite(pg.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = pos)
+        
 class CollisionSprite(pg.sprite.Sprite):
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
