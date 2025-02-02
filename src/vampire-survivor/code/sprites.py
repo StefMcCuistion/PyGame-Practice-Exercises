ce4119f574f31e17473f15eedac1452b1fe16817
Stef McCuistion <stef.mccuistion@gmail.com>
2025-02-01 23:54:11 -0600

    Switched placement of sprites to be based on the topleft of the image rather than the center, so that they appear as they do in Tiled.

--- src/vampire-survivor/code/sprites.py
+++ src/vampire-survivor/code/sprites.py
@@ -4,10 +4,10 @@ class Sprite(pg.sprite.Sprite):
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
-        self.rect = self.image.get_frect(center = pos)
-        
+        self.rect = self.image.get_frect(topleft = pos)
+
 class CollisionSprite(pg.sprite.Sprite):
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
-        self.rect = self.image.get_frect(center = pos)
+        self.rect = self.image.get_frect(topleft = pos)
