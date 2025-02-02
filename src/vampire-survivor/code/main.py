ca19c1039b5a07f58d52fb0d68fdeffe72b3d5f3
Stef McCuistion <stef.mccuistion@gmail.com>
2025-02-01 22:16:26 -0600

    Added init and run functions to the Game class.

--- src/vampire-survivor/code/main.py
+++ src/vampire-survivor/code/main.py
@@ -1,42 +1,48 @@
 from settings import *
-from player import *
+from player import Player
 
 class Game():
-    # Setup
-    pg.init()
-    display = pg.display.set_mode((W, H))
-    pg.display.set_caption('Vampire Survivor Clone')
-    clock = pg.time.Clock()
-    running = True
+    def __init__(self):
+        # Setup
+        pg.init()
+        self.display = pg.display.set_mode((W, H))
+        pg.display.set_caption('Vampire Survivor Clone')
+        self.clock = pg.time.Clock()
+        self.running = True
 
-    # Imports
-    player_up_frames = [pg.image.load(join('..', 'images', 'player', 'up', f'{i}.png')).convert_alpha() for i in range(4)]
-    player_right_frames = [pg.image.load(join('..', 'images', 'player', 'right', f'{i}.png')).convert_alpha() for i in range(4)]
-    player_down_frames = [pg.image.load(join('..', 'images', 'player', 'down', f'{i}.png')).convert_alpha() for i in range(4)]
-    player_left_frames = [pg.image.load(join('..', 'images', 'player', 'left', f'{i}.png')).convert_alpha() for i in range(4)]
+        # Imports
+        self.player_up_frames = [pg.image.load(join('..', 'images', 'player', 'up', f'{i}.png')).convert_alpha() for i in range(4)]
+        self.player_right_frames = [pg.image.load(join('..', 'images', 'player', 'right', f'{i}.png')).convert_alpha() for i in range(4)]
+        self.player_down_frames = [pg.image.load(join('..', 'images', 'player', 'down', f'{i}.png')).convert_alpha() for i in range(4)]
+        self.player_left_frames = [pg.image.load(join('..', 'images', 'player', 'left', f'{i}.png')).convert_alpha() for i in range(4)]
 
-    # Sprites
-    all_sprites = pg.sprite.Group()
-    player = Player((W / 2, H / 2), player_down_frames, all_sprites)
+        # Groups
+        self.all_sprites = pg.sprite.Group()
 
-    while running:
-        dt = clock.tick() / 1000
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_ESCAPE:
-                    running = False
+        # Sprites
+        self.player = Player((W / 2, H / 2), self.player_down_frames, self.all_sprites)
 
-        # Update
-        all_sprites.update(dt)
+    def run(self):
 
-        # Draw the game
-        display.fill('gray')
-        all_sprites.draw(display)
+        while self.running:
+            dt = self.clock.tick() / 1000
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        self.running = False
 
-        pg.display.flip()
+            # Update
+            self.all_sprites.update(dt)
+
+            # Draw the game
+            self.display.fill('gray')
+            self.all_sprites.draw(self.display)
+            pg.display.flip()
+
     pg.quit()
 
 if __name__ == '__main__':
     game = Game()
+    game.run()
--- src/vampire-survivor/code/player.py
+++ src/vampire-survivor/code/player.py
@@ -7,20 +7,28 @@ class Player(pg.sprite.Sprite):
         self.frame_idx = 0
         self.image = self.frames[self.frame_idx]
         self.rect = self.image.get_frect(center = pos)
+
+        # Movement
         self.dir = pg.math.Vector2()
         self.speed = 300
 
     def update(self, dt):
-        keys = pg.key.get_pressed()
-        self.dir.x = int(keys[pg.K_RIGHT]) - int(keys[pg.K_LEFT])
-        self.dir.y = int(keys[pg.K_DOWN]) - int(keys[pg.K_UP])
-        self.dir = self.dir.normalize() if self.dir else self.dir
-        self.rect.center += self.dir * self.speed * dt
+        self.input()
+        self.movement(dt)
         if self.dir:
             self.frame_idx += 5 * dt
         else: 
             self.frame_idx = 0
         self.image = self.frames[int(self.frame_idx) % len(self.frames)]
+
+    def movement(self, dt):
+        self.rect.center += self.dir * self.speed * dt
+
+    def input(self):
+        keys = pg.key.get_pressed()
+        self.dir.x = int(keys[pg.K_RIGHT]) - int(keys[pg.K_LEFT])
+        self.dir.y = int(keys[pg.K_DOWN]) - int(keys[pg.K_UP])
+        self.dir = self.dir.normalize() if self.dir else self.dir
 
 
     
